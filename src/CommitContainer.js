4fe390daf5ab8c37e86b63cf71fc5e0f43e6f684
hitheshr <hitheshr@virtusense.com>
2023-07-12 00:01:31 -0500

    storing github auth

--- src/CommitContainer.js
+++ src/CommitContainer.js
@@ -7,7 +7,12 @@ const CommitContainer = ({ username, repo, token }) => {
 
   useEffect(() => {
     if (username && repo && token) {
-      fetchCommits(username, repo, token);
+      var currentUrl = window.location.href;
+      var splitUrl = currentUrl.split('/');
+      var _username = splitUrl[4]
+      var _repo = splitUrl[5];
+      console.log(_username, _repo);
+      fetchCommits(_username, _repo, token);
     }
   }, [username, repo, token]);
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CommitContainer from './CommitContainer';
 import { auth } from './firebase';
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { getLocalToken } from './gitgraph/getLocalToken';
 
 const HomePage = () => {
   const [username, setUsername] = useState('opencv'); // Set default username
@@ -11,16 +12,42 @@ const HomePage = () => {
 
   const [token, setToken] = useState(null);
 
-  const signInWithGithub = () => {
-    const provider = new GithubAuthProvider();
-    provider.addScope('repo');
-    signInWithPopup(auth, provider).then((result) => {
-      console.log(result);
-      setToken(result._tokenResponse.oauthAccessToken);
-    }).catch((error) => {
-      console.error(error);
-      // handle error
-    });
+  const checkTokenValidity = async (token) => {
+    try {
+      const response = await fetch('https://api.github.com/user', {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        // The token is valid
+        return true;
+      } else {
+        // The token is not valid
+        return false;
+      }
+    } catch (error) {
+      // Handle error
+    }
+  };
+
+  const signInWithGithub = async () => {
+    var localToken = getLocalToken();
+    const isValidToken = await checkTokenValidity(localToken);
+    
+    if (!isValidToken) {
+      const provider = new GithubAuthProvider();
+      provider.addScope('repo');
+      signInWithPopup(auth, provider).then((result) => {
+        console.log(result);
+        setToken(result._tokenResponse.oauthAccessToken);
+      }).catch((error) => {
+        console.error(error);
+        // handle error
+      });
+    }else{
+      setToken(localToken);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -33,9 +60,7 @@ const HomePage = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="GitHub username" />
-        <input value={repo} onChange={(e) => setRepo(e.target.value)} placeholder="GitHub repository" />
-        <button type="submit">Sign in with GitHub and Submit</button>
+        <button type="submit">See graph</button>
       </form>
       {token && <CommitContainer username={submittedUsername} repo={submittedRepo} token={token} />}
     </div>
